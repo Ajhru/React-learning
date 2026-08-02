3c2f76b12fbf9b8e9b7af38a42ddf84f8c40baa0
Ajhru khan <akay9571@gmail.com>
2026-08-02 11:02:50 +0530

    Project setup

---
+++ BlogWriter/src/env_data/DataBase.js
@@ -0,0 +1,9 @@
+export const Data = {
+
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    projectId: String(import.meta.env.VITE_PROJECT_ID),
+    databaseId: String(import.meta.env.VITE_DATABASE_ID),
+    collectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    buketId: String(import.meta.env.VITE_BUKET_ID),
+
+}
